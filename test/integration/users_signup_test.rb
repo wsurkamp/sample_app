5ab1265b8d60263010119dc434f5f42ed7b0de83
Christian W. Surkamp <christianwkamp1@gmail.com>
2015-09-14 20:50:39 -0300

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
- 
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -22,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
