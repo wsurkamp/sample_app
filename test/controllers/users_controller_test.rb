a07178262175453d1dadd0e635f59c5924008513
Christian W. Surkamp <christianwkamp1@gmail.com>
2015-09-23 23:51:38 -0300

    Exercises chapter 9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              FILL_IN,
+                                            password_confirmation: FILL_IN,
+                                            admin: FILL_IN }
+    assert_not @other_user.FILL_IN.admin?
+  end
+
 end
